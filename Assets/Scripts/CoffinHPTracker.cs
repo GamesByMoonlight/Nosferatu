dd979eaddbecd40c487fb92a9c15c11b6e6d5360
Lance <lance.bailey@gmail.com>
2018-07-01 18:14:17 -0500

    * Coffin can now run out of HP properly (although nothing really happens yet) * Likely needs to be tested with many different bullet types (not sure how good the colliders are)

--- Assets/Scripts/CoffinHPTracker.cs
+++ Assets/Scripts/CoffinHPTracker.cs
@@ -1,11 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class CoffinHPTracker : MonoBehaviour {
+public class CoffinHPTracker : NetworkBehaviour {
 
-    public AttributesObject attributes;
+    [SerializeField]
+    private AttributesObject CoffinAttributes;
+    private Attributes attributes = new Attributes();
 
+    public NetworkHealthController networkHealthController;
+
+    private void Awake()
+    {
+        CoffinAttributes.Initialize(attributes);
+        GetComponent<NetworkHealthController>().ForGameObject = attributes;
+    }
 
 }
 

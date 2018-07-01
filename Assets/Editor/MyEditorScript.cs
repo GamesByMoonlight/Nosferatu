c4fcb834b4029b69fea28712bf3b3a01075e5b2b
luis davila <ldavila07@icloud.com>
2018-07-01 16:44:08 -0500

    added editor script

---
+++ Assets/Editor/MyEditorScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+public class MyEditorScript
+{
+	public static void PerformBuild ()
+	{
+        string[] scenes = { "Assets/_Scenes/Lobby (offline).unity" };
+		BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+		buildPlayerOptions.scenes = scenes;
+		buildPlayerOptions.locationPathName = "Build/windows/Windows.exe";
+		buildPlayerOptions.target = BuildTarget.StandaloneWindows;
+		buildPlayerOptions.options = BuildOptions.None;
+		BuildPipeline.BuildPlayer(buildPlayerOptions);
+	}
+
+	public static void PerformOSXBuild ()
+	{
+        string[] scenes = { "Assets/_Scenes/Lobby (offline).unity" };
+		BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+		buildPlayerOptions.scenes = scenes;
+		buildPlayerOptions.locationPathName = "Build/osx/OSX.app";
+		buildPlayerOptions.target = BuildTarget.StandaloneOSX;
+		buildPlayerOptions.options = BuildOptions.None;
+		BuildPipeline.BuildPlayer(buildPlayerOptions);
+	}
+}

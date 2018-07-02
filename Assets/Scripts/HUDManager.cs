c6d2d96d248d4594bebe6c8998a52f34db427f3f
2percenter <travis.c.hawks@gmail.com>
2018-07-01 22:34:54 -0500

    -Roughing out HUD

---
+++ Assets/Scripts/HUDManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public enum HUDState { none, gameStart, gamePlay, gameOver } //Doing this for now. Not the best method, probably.
+
+[RequireComponent(typeof(Animator))]
+public class HUDManager : MonoBehaviour
+{
+
+    private HUDState state;
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        state = HUDState.gameStart;
+        OpenHUD();
+    }
+
+    private void OpenHUD()
+    {
+        animator.SetTrigger("appear");
+    }
+
+
+}

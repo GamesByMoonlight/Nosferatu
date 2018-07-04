d77cdf1c4cf241f3b3ebb9591f15e333a433847e
Lance <lance.bailey@gmail.com>
2018-07-04 15:37:55 -0500

    * Made two dummy objects to test buff locations

---
+++ Assets/BuffSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuffSpawner : MonoBehaviour {
+
+    public GameObject[] buffsAvailable;
+
+    Transform[] BuffSpawnTransforms;
+
+
+	// Use this for initialization
+	void Start () {
+        BuffSpawnTransforms = GetComponentsInChildren<Transform>();
+
+        foreach (Transform buffLocation in BuffSpawnTransforms)
+        {
+            int index = Random.Range(0, buffsAvailable.Length);
+
+            Instantiate(buffsAvailable[index], buffLocation.transform.position, Quaternion.identity);
+        }
+	}
+	
+}

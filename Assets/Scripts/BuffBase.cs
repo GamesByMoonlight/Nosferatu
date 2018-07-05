a191e1e286be75561e1935788df1d67956e9b70b
Zaneeverett <36215239+Zaneeverett@users.noreply.github.com>
2018-07-05 11:32:27 -0500

    Feature/19 buffs (#42)
    * Buff Prefabs for Atk, Spd, and HP.
    
    * Buff SO setup properly and added to Buff spawner

---
+++ Assets/Scripts/BuffBase.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BuffBase : MonoBehaviour
+{
+    public AttributesObject Attributes;
+    private Attributes attr = new Attributes();
+
+    protected virtual void Awake()
+    {
+        Attributes.Initialize(attr);
+    }
+}
